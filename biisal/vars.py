f8ef09f9e636a7dfb744d938059c90be16c4e338
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2024-07-08 10:20:26 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -36,7 +36,7 @@ class Var(object):
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
+        URL = "http://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://wiciya3691:IUmgECrTO5ZGIzP9@cluster0.uu8csuf.mongodb.net/?retryWrites=true&w=majority'))
