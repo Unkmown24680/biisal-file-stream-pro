0f154478392de536b7b5413d42793225114a3781
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2024-08-04 13:44:55 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -3,8 +3,6 @@ import os
 from os import getenv, environ
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
 bisal_channel = "https://t.me/movierulzking"
@@ -29,16 +27,15 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'unknown_0501'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
-    
+        APP_NAME = str(getenv('APP_NAME'))  # dont need to fill anything here
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',True))
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME + '.herokuapp.com'
+    HAS_SSL = bool(getenv('HAS_SSL', True))
     if HAS_SSL:
-        URL = "https://{web-3crir7qcd23c.up-de-fra1-k8s-1.apps.run-on-seenode.com}/".format(FQDN)
+        URL = "https://{}/".format(FQDN)
     else:
-        URL = "http://{web-3crir7qcd23c.up-de-fra1-k8s-1.apps.run-on-seenode.com}/".format(FQDN)
+        URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://wiciya3691:IUmgECrTO5ZGIzP9@cluster0.uu8csuf.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'movierulzking'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
